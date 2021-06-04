671ecd3db569d4a5522e7dc1bae205eb3548e0d3
Michel SADEU <michel.sadeu-ngakou@neel.cnrs.fr>
2021-06-04 16:49:08 +0200

    Ajoût du model de données adresse dans models

---
+++ models/adresse.js
@@ -0,0 +1,11 @@
+exports.Adresse = class {
+    constructor(idA, num, typeVoie, nomVoie, codePostal, ville, pays) {
+        this.idA = idA;
+        this.num = num;
+        this.typeVoie = typeVoie;
+        this.nomVoie = nomVoie;
+        this.codePostal = codePostal;
+        this.ville = ville;
+        this.pays = pays;
+    }
+}
