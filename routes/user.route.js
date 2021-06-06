cc1acd62906c63b1c8044b88974d41c726cce2b9
Michel SADEU <michel.sadeu-ngakou@neel.cnrs.fr>
2021-06-06 14:06:02 +0200

    implétentation de la route de connexion dans user.route.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 
 router.post('/signup', userController.creerUser);
+router.post('/login', userController.connexion);
 
 
 module.exports = router;
