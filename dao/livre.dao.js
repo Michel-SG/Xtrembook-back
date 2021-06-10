d8e517f019dcccc89fd5d35e355c2b5c67198c4c
Scara84 <antho.scara@yahoo.fr>
2021-06-10 15:46:53 +0200

    seul auteur est à finaliser

--- dao/livre.dao.js
+++ dao/livre.dao.js
@@ -3,8 +3,8 @@ const connection = require("../database.js");
 exports.getOneById = (referenceArticle) => {
   return new Promise((resolve, reject) => {
     const req = connection.query(
-      "SELECT referenceArticle, titre, resumed, isbn13 FROM article, livre WHERE livre.livreArticle = ? AND article.referenceArticle = ?",
-      [referenceArticle, referenceArticle],
+      "SELECT referenceArticle, titre, resumed, isbn13, imageUrl, prixUnit, formatLivre, genre, nomEditeur FROM Article LEFT JOIN Livre ON Article.referenceArticle = Livre.livreArticle LEFT JOIN Genre ON Livre.livreGenre = Genre.IdG LEFT JOIN Editeur ON Livre.livreEditeur = Editeur.IdE WHERE article.referenceArticle = ?",
+      referenceArticle,
       (err, result) => {
         console.log(req.sql);
         err ? reject(err) : resolve(result);
